4956237ad139e8c2abfb8df5bb55048d68354498
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2021-08-24 17:26:30 -0300

    Alterações no nome das variáveis, finalização das colisões

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 function processarJogo() {
+    //=================
     //Criar elementos na tela
+    //=================
+
     $(".inicio").hide()
     $(".fundo-jogo").append("<div class='jogador animacao-jogador'</div>")
-    $(".fundo-jogo").append("<div class='amigo animacao-amigo'</div>")
-    $(".fundo-jogo").append("<div class='inimigo1 animacao-inimigo1'</div>")
-    $(".fundo-jogo").append("<div class='inimigo2'</div>")
+    $(".fundo-jogo").append("<div class='aliado animacao-aliado'</div>")
+    $(".fundo-jogo").append("<div class='helicoptero-inimigo animacao-helicoptero-inimigo'</div>")
+    $(".fundo-jogo").append("<div class='caminhao'</div>")
 
+    //=================
     //Game looping
+    //=================
     const jogo = {}
     jogo.temporizador = setInterval(executarLooping, 30)
 
     function executarLooping() {
         moverFundo()
         moverJogador()
-        moverInimigo1()
-        moverInimigo2()
-        moverAmigo()
+        moverHelicopteroInimigo()
+        moverCaminhao()
+        moverAliado()
         detectarColisoes()
     }
 
@@ -24,7 +29,9 @@ function processarJogo() {
         $(".fundo-jogo").css("background-position", esquerda - 1)
     }
 
+    //=================
     //Movimentação do jogador:
+    //=================
     const pressionarTecla = []
     const tecla = {
         W: 87,
@@ -64,40 +71,46 @@ function processarJogo() {
         }
     }
 
+    //=================
     //Movimentação dos inimigos
+    //=================
     let posicaoY = parseInt(Math.random() * 334)
-    function moverInimigo1() {
-        let posicaoX = parseInt($('.inimigo1').css('left'))
-        $(".inimigo1").css('left', posicaoX - 7)
-        $(".inimigo1").css('top', posicaoY)
+    function moverHelicopteroInimigo() {
+        let posicaoX = parseInt($('.helicoptero-inimigo').css('left'))
+        $(".helicoptero-inimigo").css('left', posicaoX - 7)
+        $(".helicoptero-inimigo").css('top', posicaoY)
 
         if (posicaoX <= 0) {
             posicaoY = parseInt(Math.random() * 334)
-            $(".inimigo1").css('left', 694)
-            $(".inimigo1").css('top', posicaoY)
+            $(".helicoptero-inimigo").css('left', 694)
+            $(".helicoptero-inimigo").css('top', posicaoY)
         }
     }
 
-    function moverInimigo2() {
-        let posicaoX = parseInt($('.inimigo2').css('left'))
-        $(".inimigo2").css('left', posicaoX - 3)
+    function moverCaminhao() {
+        let posicaoX = parseInt($('.caminhao').css('left'))
+        $(".caminhao").css('left', posicaoX - 3)
 
         if (posicaoX <= 0) {
-            $(".inimigo2").css('left', 775)
+            $(".caminhao").css('left', 775)
         }
     }
 
-    //movimentação do amigo
-    function moverAmigo() {
-        let posicaoX = parseInt($('.amigo').css('left'))
-        $(".amigo").css('left', posicaoX + 1)
+    //=================
+    //movimentação do aliado
+    //=================
+    function moverAliado() {
+        let posicaoX = parseInt($('.aliado').css('left'))
+        $(".aliado").css('left', posicaoX + 1)
 
         if (posicaoX > 906) {
-            $('.amigo').css('left', 0)
+            $('.aliado').css('left', 0)
         }
     }
 
+    //=================
     //disparo do projétil
+    //=================
     let podeAtirar = true
     function criarProjetil() {
         if (podeAtirar === true) {
@@ -124,35 +137,123 @@ function processarJogo() {
                 podeAtirar = true
             }
         }
-    } //function criarProjetil
-
+    }
+    //=================
     //Colisões
+    //=================
     function detectarColisoes() {
-        let colisaoJogadorInimigo1 = ($(".jogador").collision($(".inimigo1")))
+        let colisaoJogadorHelicopteroInimigo = ($(".jogador").collision($(".helicoptero-inimigo")))
+        let colisaoJogadorCaminhao = ($(".jogador").collision($(".caminhao")))
+        let colisaoProjetilHelicopteroInimigo = ($(".projetil").collision($(".helicoptero-inimigo")))
+        let colisaoProjetilCaminhao = ($(".projetil").collision($(".caminhao")))
+        let colisaoJogadorAliado = ($(".jogador").collision($(".aliado")))
+        let colisaoAliadoCaminhao = ($(".aliado").collision($(".caminhao")))
 
-        if (colisaoJogadorInimigo1.length > 0) {
-            let inimigo1X = parseInt($('.inimigo1').css('left'))
-            let inimigo1Y = parseInt($('.inimigo1').css('top'))
-            explosaoJogadorInimigo1(inimigo1X, inimigo1Y)
+        if (colisaoJogadorHelicopteroInimigo.length > 0) {
+            let helicopteroInimigoX = parseInt($('.helicoptero-inimigo').css('left'))
+            let helicopteroInimigoY = parseInt($('.helicoptero-inimigo').css('top'))
+            explosaoHelicopteroInimigo(helicopteroInimigoX, helicopteroInimigoY)
 
             posicaoY = parseInt(Math.random() * 334)
-            $('.inimigo1').css('left', 694)
-            $('.inimigo1').css('top', posicaoY)
+            $('.helicoptero-inimigo').css('left', 694)
+            $('.helicoptero-inimigo').css('top', posicaoY)
+        }
+
+        if (colisaoJogadorCaminhao.length > 0) {
+            let caminhaoX = parseInt($('.caminhao').css('left'))
+            let caminhaoY = parseInt($('.caminhao').css('top'))
+            explosaoCaminhao(caminhaoX, caminhaoY)
+
+            $('.caminhao').remove()
+
+            reposicionarCaminhao()
+        }
+
+        if (colisaoProjetilHelicopteroInimigo.length > 0) {
+            let helicopteroInimigoX = parseInt($('.helicoptero-inimigo').css('left'))
+            let helicopteroInimigoY = parseInt($('.helicoptero-inimigo').css('top'))
+            explosaoHelicopteroInimigo(helicopteroInimigoX, helicopteroInimigoY)
+            $('.projetil').css('left', 950)
+
+            posicaoY = parseInt(Math.random() * 334)
+            $('.helicoptero-inimigo').css('left', 694)
+            $('.helicoptero-inimigo').css('top', posicaoY)
+        }
+
+        if (colisaoProjetilCaminhao.length > 0) {
+            let caminhaoX = parseInt($('.caminhao').css('left'))
+            let caminhaoY = parseInt($('.caminhao').css('top'))
+            explosaoCaminhao(caminhaoX, caminhaoY)
+            $('.projetil').css('left', 950)
+
+            $('.caminhao').remove()
+
+            reposicionarCaminhao()
+        }
+
+        if (colisaoJogadorAliado.length > 0) {
+            reposicionarAliado()
+            $('.aliado').remove()
         }
     }
 
-    function explosaoJogadorInimigo1(inimigo1X, inimigo1Y) {
-        $('.fundo-jogo').append("<div class='explosao-jogador-inimigo1'></div>")
-        
-        $('.explosao-jogador-inimigo1').css('top', inimigo1Y)
-        $('.explosao-jogador-inimigo1').css('left', inimigo1X)
-        $('.explosao-jogador-inimigo1').animate({width:200, opacity:0}, "slow")  
+    function explosaoHelicopteroInimigo(helicopteroInimigoX, helicopteroInimigoY) {
+        $('.fundo-jogo').append("<div class='explosao-helicoptero-inimigo'></div>")
+
+        $('.explosao-helicoptero-inimigo').css('top', helicopteroInimigoY)
+        $('.explosao-helicoptero-inimigo').css('left', helicopteroInimigoX)
+        $('.explosao-helicoptero-inimigo').animate({ width: 200, opacity: 0 }, "slow")
 
         let tempoExplosao = window.setInterval(removerExplosao, 1000)
 
         function removerExplosao() {
-            $('.explosao-jogador-inimigo1').remove()
+            $('.explosao-helicoptero-inimigo').remove()
             window.clearInterval(tempoExplosao)
         }
     }
-} //function iniciarJogo()
+
+    function explosaoCaminhao(caminhaoX, caminhaoY) {
+        $('.fundo-jogo').append("<div class='explosao-caminhao'></div>")
+
+        $('.explosao-caminhao').css('top', caminhaoY)
+        $('.explosao-caminhao').css('left', caminhaoX)
+        $('.explosao-caminhao').animate({ width: 200, opacity: 0 }, "slow")
+
+        let tempoExplosao = window.setInterval(removerExplosao, 1000)
+
+        function removerExplosao() {
+            $('.explosao-caminhao').remove()
+            window.clearInterval(tempoExplosao)
+        }
+    }
+
+    function reposicionarCaminhao() {
+        let tempoColisaoCaminhao = window.setInterval(aparecerCaminhao, 5000)
+
+        function aparecerCaminhao() {
+            window.clearInterval(tempoColisaoCaminhao)
+
+            if (fimDeJogo == false) {
+                $('.fundo-jogo').append("<div class='caminhao'></div>")
+            }
+        }
+    }
+
+    function reposicionarAliado() {
+        let tempoColisaoAliado = window.setInterval(aparecerAliado, 6000)
+
+        function aparecerAliado() {
+            window.clearInterval(tempoColisaoAliado)
+
+            if (fimDeJogo == false) {
+                $('.fundo-jogo').append("<div class='aliado animacao-aliado'></div>")
+            }
+        }
+    }
+
+
+    //=================
+    //Fim de jogo
+    //=================
+    let fimDeJogo = false
+}
